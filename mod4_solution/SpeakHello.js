070f0f07a54618ac555b6dd1fc252373d245f829
sh3llgh0st <ben.vanaken@gmail.com>
2017-02-11 20:16:55 +0100

    Add files via upload

---
+++ mod4_solution/SpeakHello.js
@@ -0,0 +1,12 @@
+(function(window){
+    var helloSpeaker = {};
+    helloSpeaker.speak = function(name){
+        console.log(speakWord + " " + name);
+    };
+    var speakWord = "Hello";
+
+    window.helloSpeaker = helloSpeaker;
+
+})(window);
+
+
